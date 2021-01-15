098bd65753914f3e4b76a1d8e015e2bf7778e68e
denpolischuk <den.polischuk19@gmail.com>
2021-01-15 21:48:01 +0100

    chore(init): upd prettier

--- main.js
+++ main.js
@@ -3,35 +3,35 @@ const url = require('url')
 const { app, BrowserWindow } = require('electron')
 
 const isWatchMode =
-	process.env.NODE_ENV === 'development' && process.env.WATCH_MODE === 'true'
+  process.env.NODE_ENV === 'development' && process.env.WATCH_MODE === 'true'
 
 if (isWatchMode) {
-	require('electron-reload')(__dirname, {
-		electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-	})
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+  })
 }
 
-const getFileUrl = filePath => `file://${filePath}`
+const getFileUrl = (filePath) => `file://${filePath}`
 
 let win
 
 function createWindow() {
-	win = new BrowserWindow({
-		width: 700,
-		height: 500,
-		icon: __dirname + '/icon/icon.png'
-	})
+  win = new BrowserWindow({
+    width: 700,
+    height: 500,
+    icon: __dirname + '/icon/icon.png'
+  })
 
-	win.loadURL(getFileUrl(path.join(__dirname, 'index.html')))
-	win.webContents.openDevTools()
+  win.loadURL(getFileUrl(path.join(__dirname, 'index.html')))
+  win.webContents.openDevTools()
 
-	win.on('closed', () => {
-		win = null
-	})
+  win.on('closed', () => {
+    win = null
+  })
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-	app.quit()
+  app.quit()
 })
